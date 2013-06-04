95f1e4f03620c71ee39167c85d0671f313622667
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-06-04 16:08:18 +0200

    Rename project.

--- lib/base.sh
+++ lib/base.sh
@@ -1,5 +1,5 @@
 
-koproj_run() {
+kotam_run() {
 	local cmd="$*"
 	qdbus org.kde.konsole /Sessions/${session} sendText "$cmd"
 	qdbus org.kde.konsole /Sessions/${session} sendText "
@@ -7,14 +7,14 @@ koproj_run() {
 }
 
 
-koproj_newtab() {
+kotam_newtab() {
 	#dbus-send --session --dest=${KONSOLE_DBUS_SERVICE} --type=method_call \
 	#	--print-reply /konsole/MainWindow_1 org.kde.KMainWindow.activateAction string:"new-tab"
 
 	session=$(qdbus org.kde.konsole /Konsole newSession)
 }
 
-koproj_renametab() {
+kotam_renametab() {
 	#sessionno=$1
 	tabname=$1
 	#session="/Sessions/${sessionno}"
