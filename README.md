56c57bbe888ba6d68b3c534787bbb4726db20193
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-06-04 15:46:44 +0200

    Initial import.

---
+++ README.md
@@ -0,0 +1,8 @@
+KoProj is a tool for managing console project environments
+
+Ex: Project X require 3 terminals, with name A, B, C
+    A : must be in directory ~/
+    B : must be in directory ~/foo
+        run command X
+    C : must be in directory ~/bar
+	run command Y
---
+++ lib/base.sh
@@ -0,0 +1,24 @@
+
+koproj_run() {
+	local cmd="$*"
+	qdbus org.kde.konsole /Sessions/${session} sendText "$cmd"
+	qdbus org.kde.konsole /Sessions/${session} sendText "
+	"
+}
+
+
+koproj_newtab() {
+	#dbus-send --session --dest=${KONSOLE_DBUS_SERVICE} --type=method_call \
+	#	--print-reply /konsole/MainWindow_1 org.kde.KMainWindow.activateAction string:"new-tab"
+
+	session=$(qdbus org.kde.konsole /Konsole newSession)
+}
+
+koproj_renametab() {
+	#sessionno=$1
+	tabname=$1
+	#session="/Sessions/${sessionno}"
+	#dbus-send --session --dest=${KONSOLE_DBUS_SERVICE} --type=method_call --print-reply ${session} org.kde.konsole.Session.setTitle int32:1 string:"$tabname"
+	qdbus org.kde.konsole /Sessions/${session} setTitle 1 "$tabname"
+}
+
